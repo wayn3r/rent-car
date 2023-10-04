866e55adaa83696e5fa48383d4ec0baa927c51f5
platano8876 <britovittini@gmail.com>
2023-10-04 01:03:13 -0500

    Create EmployeeController.cs

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RentCar.Database;
+using RentCar.Database.Entities;
+
+namespace RentCar.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly RentCarContext _context;
+
+        public EmployeeController(RentCarContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Clients
+        public async Task<IActionResult> Index()
+        {
+              return _context.Employee != null ? 
+                          View(await _context.Employee.ToListAsync()) :
+                          Problem("Entity set 'RentCarContext.Clients'  is null.");
+        }
+
+        // GET: Clients/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Employee == null)
+            {
+                return NotFound();
+            }
+
+            var Employee = await _context.Employee
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (Employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(Employee);
+        }
+
+        // GET: Clients/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clients/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Cedula,tandalabor,porcientocomision,Fechaingreso,Estado")] Employee Employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(Employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Employee);
+        }
+
+        // GET: Clients/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Employee == null)
+            {
+                return NotFound();
+            }
+
+            var Employee = await _context.Employee.FindAsync(id);
+            if (Employee == null)
+            {
+                return NotFound();
+            }
+            return View(Employee);
+        }
+
+        // POST: Clients/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Cedula,tandalabor,porcientocomision,Fechaingreso,Estado")] Employee Employee)
+        {
+            if (id != Employee.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(Employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeExists(Employee.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Employee);
+        }
+
+        // GET: Clients/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Employee == null)
+            {
+                return NotFound();
+            }
+
+            var Employee = await _context.Employee
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (Employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(Employee);
+        }
+
+        // POST: Clients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Clients == null)
+            {
+                return Problem("Entity set 'RentCarContext.Clients'  is null.");
+            }
+            var Employee = await _context.Employee.FindAsync(id);
+            if (clients != null)
+            {
+                _context.Employee.Remove(clients);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeExists(int id)
+        {
+          return (_context.Employee?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
