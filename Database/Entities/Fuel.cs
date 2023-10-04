d1e21b262663453927b60867bea5cf02f511fcce
platano8876 <britovittini@gmail.com>
2023-10-04 12:01:34 -0400

    feat: add employee and fuel entities (#2)
    * Create Employee.cs
    
    * Create Fuel.cs

---
+++ Database/Entities/Fuel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentCar.Database.Entities
+{
+
+
+
+    public class Fuel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        [Display(Name = "Descripcion")]
+
+        public string description { get; set; }
+
+        [Display(Name = "Estado")]
+        public Status status { get; set; }
+
+
+    }
+}
